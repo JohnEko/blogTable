3e736f3684771f22510f88c95b5ad8227be9b835
JohnEko <efezino10@gmail.com>
2025-06-24 19:10:15 +0100

    advert box grid

--- client/nextapp/app/postadvert/advert.tsx
+++ client/nextapp/app/postadvert/advert.tsx
@@ -1,18 +1,43 @@
 import React from 'react'
+import Image from 'next/image'
 import "./advertpage.css"
 
 const PostAdvert =() => {
   return (
-    <div className='advert-item'>
-        {/* <div className='advert-box'>
+    <div>
+        <div className='advert-item'>
+            <div className='advert-box'>
+                {/* <div className='advert-item'> */}
+                    <Image 
+                        src='/villa.jpg'
+                        className="image-villa"
+                        alt="Beach"
+                        width={200}
+                        height={200}
+                    />
+            
+                {/* </div> */}
+            </div> 
+             <div className='advert-box'>
+                    <Image 
+                        src='/villa.jpg'
+                        className="image-villa"
+                        alt="Beach"
+                        width={200}
+                        height={200}
+                    />
+            </div> 
+             <div className='advert-box'>
+                    <Image 
+                        src='/villa.jpg'
+                        className="image-villa"
+                        alt="Beach"
+                        width={200}
+                        height={200}
+                    />
+            </div>  
         
-            <div className='advert-item'>
-             
-            </div>
-
-        </div> */}
-        
-        advert
+        </div>
     </div>
   )
 }
